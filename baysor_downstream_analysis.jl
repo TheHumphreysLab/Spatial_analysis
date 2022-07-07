d0fbf528113f04641b32a5d6fe0d71aa5a503d0b
Haojia Wu <haojiawu@wustl.edu>
2022-07-07 14:04:16 -0500

    Update baysor_downstream_analysis.jl

--- baysor_downstream_analysis.jl
+++ baysor_downstream_analysis.jl
@@ -66,3 +66,28 @@ using JLD2
 @save "individual_clustering_obj.jld"
 
 ## plot segmentation polygons
+df_spatial, gene_names = B.load_df("day2/segmentation.csv")
+scale=25;
+min_pixels_per_cell = 15;
+grid_step = scale / min_pixels_per_cell
+bandwidth= scale / 10
+df_spatial.ncv_color = convert(Vector{String}, df_spatial.ncv_color)
+
+import Makie as MK
+
+fig1=B.plot_molecules(df_spatial, polygons, color=:ncv_color, 
+    markersize=1, size=(5000,6000)
+);
+MK.save("day2_plot_seg_whole.png", fig1)
+fig1
+
+
+fig2=B.plot_molecules(df_spatial, polygons, color=:ncv_color, 
+    markersize=1, size=(1000,1000),
+    xlims=(11000,16000), ylims=(8500,13500)
+);
+MK.save("day2_plot_seg_crop.png", fig2)
+fig2
+
+using JLD2
+@save "segmentation_day2_ploting.jld"

e5a75a7287b24289c7374a5bc060513daec60528
Haojia Wu <haojiawu@wustl.edu>
2022-07-14 10:19:44 -0500

    Update Julia_functions_for_ploting.jl

--- Julia_functions_for_ploting.jl
+++ Julia_functions_for_ploting.jl
@@ -2,7 +2,7 @@
 function plot_genes_cell_segments(df_spatial::DataFrame, polygons::Array{Matrix{Float64}, 1}=Matrix{Float64}[]; 
         genes::Union{Vector, Symbol, String}="Podxl", 
         colors::Union{Vector, Symbol, String}="blue", 
-        bg_color::Union{Vector, Symbol, String}="gray90",
+        bg_color::Union{Vector, Symbol, String}="gray95",
         genesize =2, segline_size=0.5, 
         canvas_size=(5000,6000),x_lims=nothing, y_lims=nothing, 
         ann::Union{<:AbstractVector, Symbol, Nothing} = nothing,
@@ -62,7 +62,7 @@ function plot_genes_overlaid_cells(df_spatial::DataFrame, polygons::Array{Matrix
     for (color, ann) in zip(c_map, ann_vals)
         style_dict = (ann_colors === nothing) ? Dict() : Dict(:color => ann_colors[ann])
         MK.scatter!(df_spatial.x[annotation .== ann] .+ offset[1], df_spatial.y[annotation .== ann] .+ offset[2];
-            strokewidth=segline_size, markersize=markersize, label=ann, color=color, style_dict...)
+            strokewidth=0, markersize=markersize, label=ann, color=color, style_dict...)
     end
     if show_legend
         MK.axislegend(;legend_kwargs...)
@@ -70,7 +70,7 @@ function plot_genes_overlaid_cells(df_spatial::DataFrame, polygons::Array{Matrix
     MK.poly!([MK.Point2.(eachrow(p .+ [offset[1] offset[2]])) for p in polygons]; strokecolor="black", color="transparent", strokewidth=segline_size, label="")
     colors2 = df_spatial[!,:new_color]
     MK.scatter!(df_spatial.x .+ offset[1], df_spatial.y .+ offset[2]; color=colors2,
-            strokewidth=segline_size, markersize=markersize)
+            strokewidth=0, markersize=markersize)
     MK.xlims!(MK.current_axis(), xlims .+ offset[1])
     MK.ylims!(MK.current_axis(), ylims .+ offset[2])
     return MK.current_figure()

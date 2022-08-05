ddbcd73887bfc8ce2020d781edce1c08daab7a05
Haojia Wu <haojiawu@wustl.edu>
2022-08-05 15:09:44 -0500

    Create FigureS3.jl

---
+++ FigureS3.jl
@@ -0,0 +1,12 @@
+using Pkg, StatsBase, CSV, DataFrames, VegaLite, Colors, ColorSchemes, JLD2
+import CairoMakie as MK
+import SpaData as spd
+using PyCall
+using Downloads
+ENV["PYTHON"]="/bin/miniconda3/bin/python"
+Pkg.build("PyCall")
+sc = pyimport("scanpy")
+
+Downloads.download("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE139107&format=file&file=GSE139107%5FMouseIRI%2Emetadata%2Etxt%2Egz", "IRI_metadata.txt.tar.gz")
+sc_meta=CSV.File("IRI_metadata.txt.tar.gz") |> DataFrame
+Downloads.download("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE139107&format=file&file=GSE139107%5FMouseIRI%5Fcontrol%2Edge%2Etxt%2Egz", "sham.txt.tar.gz")

b2996c9a9d5ada41721a2b4281538272117de2bc
Haojia Wu <haojiawu@wustl.edu>
2022-08-05 15:31:07 -0500

    Update FigureS3.jl

--- FigureS3.jl
+++ FigureS3.jl
@@ -8,6 +8,8 @@ Pkg.build("PyCall")
 sc = pyimport("scanpy")
 
 Downloads.download("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE139107&format=file&file=GSE139107%5FMouseIRI%2Emetadata%2Etxt%2Egz", "IRI_metadata.txt.tar.gz")
-sc_meta=CSV.File("IRI_metadata.txt.tar.gz"; header=2) |> DataFrame
+sc_meta=CSV.File("IRI_metadata.txt.tar.gz") |> DataFrame
+rename!(sc_meta,append!(["Column1"], names(sc_meta))[1:end-1])
 Downloads.download("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE139107&format=file&file=GSE139107%5FMouseIRI%5Fcontrol%2Edge%2Etxt%2Egz", "sham.txt.tar.gz")
 sc_count=CSV.File("sham.txt.tar.gz"; header=2) |> DataFrame
+rename!(sc_count,append!(["Column1"], names(sc_count))[1:end-1])

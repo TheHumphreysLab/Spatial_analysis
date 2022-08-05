5f2110294bb67584df732631190b0ceb5b1b81e9
Haojia Wu <haojiawu@wustl.edu>
2022-08-05 15:18:25 -0500

    Update FigureS3.jl

--- FigureS3.jl
+++ FigureS3.jl
@@ -8,5 +8,6 @@ Pkg.build("PyCall")
 sc = pyimport("scanpy")
 
 Downloads.download("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE139107&format=file&file=GSE139107%5FMouseIRI%2Emetadata%2Etxt%2Egz", "IRI_metadata.txt.tar.gz")
-sc_meta=CSV.File("IRI_metadata.txt.tar.gz") |> DataFrame
+sc_meta=CSV.File("IRI_metadata.txt.tar.gz"; header=2) |> DataFrame
 Downloads.download("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE139107&format=file&file=GSE139107%5FMouseIRI%5Fcontrol%2Edge%2Etxt%2Egz", "sham.txt.tar.gz")
+sc_count=CSV.File("sham.txt.tar.gz"; header=2) |> DataFrame

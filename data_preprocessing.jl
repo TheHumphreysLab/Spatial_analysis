40114f0dc40920e76f174a335028a27d7b0b617d
Haojia Wu <haojiawu@wustl.edu>
2023-03-18 19:12:48 -0500

    Create data_preprocessing.jl

---
+++ data_preprocessing.jl
@@ -0,0 +1,151 @@
+import CellScopes as cs
+using CSV, DataFrames
+using SparseArrays
+
+### 1. sham ###
+count_molecules =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Sham_Jia/segmentation.csv"))
+count_df =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Sham_Jia/segmentation_counts.tsv"))
+count_cells =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Sham_Jia/segmentation_cell_stats.csv"))
+import Baysor as B
+scale=30
+min_pixels_per_cell = 15
+grid_step = scale / min_pixels_per_cell
+bandwidth= scale / 10
+polygons = B.boundary_polygons(count_molecules, count_molecules.cell, grid_step=grid_step, bandwidth=bandwidth)
+genes = count_df.gene
+cells = string.(names(count_df)[2:end])
+count_df = count_df[!, 2:end]
+count_df = convert(SparseMatrixCSC{Int64, Int64},Matrix(count_df))
+raw_count = cs.RawCountObject(count_df, cells, genes)
+count_molecules.cell = string.(count_molecules.cell)
+count_cells.cell = string.(count_cells.cell)
+sham = cs.CartanaObject(count_molecules, count_cells, raw_count;
+    prefix = "sham", min_gene = 0, min_cell = 3)
+sham = cs.normalize_object(sham)
+sham = cs.scale_object(sham)
+sham = cs.find_variable_genes(sham; nFeatures=100)
+sham = cs.run_pca(sham;  method=:svd, pratio = 1, maxoutdim = 20)
+sham.metaData.cluster = string.(sham.spmetaData.cell.cluster)
+sham.polygonData = polygons
+sham = cs.polygons_cell_mapping(sham);
+sham = cs.generate_polygon_counts(sham);
+cs.save(sham; filename="sham.jld2")
+
+### 2. IRI4h ###
+count_molecules =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Hour4_Jia/segmentation.csv"))
+count_df =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Hour4_Jia/segmentation_counts.tsv"))
+count_cells =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Hour4_Jia/segmentation_cell_stats.csv"))
+import Baysor as B
+scale=30
+min_pixels_per_cell = 15
+grid_step = scale / min_pixels_per_cell
+bandwidth= scale / 10
+polygons = B.boundary_polygons(count_molecules, count_molecules.cell, grid_step=grid_step, bandwidth=bandwidth)
+genes = count_df.gene
+cells = string.(names(count_df)[2:end])
+count_df = count_df[!, 2:end]
+count_df = convert(SparseMatrixCSC{Int64, Int64},Matrix(count_df))
+raw_count = cs.RawCountObject(count_df, cells, genes)
+count_molecules.cell = string.(count_molecules.cell)
+count_cells.cell = string.(count_cells.cell)
+hour4 = cs.CartanaObject(count_molecules, count_cells, raw_count;
+    prefix = "hour4", min_gene = 0, min_cell = 3)
+hour4 = cs.normalize_object(hour4)
+hour4 = cs.scale_object(hour4)
+hour4 = cs.find_variable_genes(hour4; nFeatures=100)
+hour4 = cs.run_pca(hour4;  method=:svd, pratio = 1, maxoutdim = 20)
+hour4.metaData.cluster = string.(hour4.spmetaData.cell.cluster)
+hour4.polygonData = polygons
+hour4 = cs.polygons_cell_mapping(hour4)
+hour4 = cs.generate_polygon_counts(hour4)
+cs.save(hour4; filename="hour4.jld2")
+
+### 3. IRI12h ###
+count_molecules =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Hour12_Jia/segmentation.csv"))
+count_df =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Hour12_Jia/segmentation_counts.tsv"))
+count_cells =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Hour12_Jia/segmentation_cell_stats.csv"))
+import Baysor as B
+scale=30
+min_pixels_per_cell = 15
+grid_step = scale / min_pixels_per_cell
+bandwidth= scale / 10
+polygons = B.boundary_polygons(count_molecules, count_molecules.cell, grid_step=grid_step, bandwidth=bandwidth)
+genes = count_df.gene
+cells = string.(names(count_df)[2:end])
+count_df = count_df[!, 2:end]
+count_df = convert(SparseMatrixCSC{Int64, Int64},Matrix(count_df))
+raw_count = cs.RawCountObject(count_df, cells, genes)
+count_molecules.cell = string.(count_molecules.cell)
+count_cells.cell = string.(count_cells.cell)
+hour12 = cs.CartanaObject(count_molecules, count_cells, raw_count;
+    prefix = "hour12", min_gene = 0, min_cell = 3)
+hour12 = cs.normalize_object(hour12)
+hour12 = cs.scale_object(hour12)
+hour12 = cs.find_variable_genes(hour12; nFeatures=100)
+hour12 = cs.run_pca(hour12;  method=:svd, pratio = 1, maxoutdim = 20)
+hour12.metaData.cluster = string.(hour12.spmetaData.cell.cluster)
+hour12.polygonData = polygons
+hour12 = cs.polygons_cell_mapping(hour12)
+hour12 = cs.generate_polygon_counts(hour12)
+cs.save(hour12; filename="hour12.jld2")
+
+### 4. IRI2d ###
+count_molecules =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Day2_Jia/segmentation.csv"))
+count_df =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Day2_Jia/segmentation_counts.tsv"))
+count_cells =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Day2_Jia/segmentation_cell_stats.csv"))
+import Baysor as B
+scale=30
+min_pixels_per_cell = 15
+grid_step = scale / min_pixels_per_cell
+bandwidth= scale / 10
+polygons = B.boundary_polygons(count_molecules, count_molecules.cell, grid_step=grid_step, bandwidth=bandwidth)
+genes = count_df.gene
+cells = string.(names(count_df)[2:end])
+count_df = count_df[!, 2:end]
+count_df = convert(SparseMatrixCSC{Int64, Int64},Matrix(count_df))
+raw_count = cs.RawCountObject(count_df, cells, genes)
+count_molecules.cell = string.(count_molecules.cell)
+count_cells.cell = string.(count_cells.cell)
+day2 = cs.CartanaObject(count_molecules, count_cells, raw_count;
+    prefix = "day2", min_gene = 0, min_cell = 3)
+day2 = cs.normalize_object(day2)
+day2 = cs.scale_object(day2)
+day2 = cs.find_variable_genes(day2; nFeatures=100)
+day2 = cs.run_pca(day2;  method=:svd, pratio = 1, maxoutdim = 20)
+day2.metaData.cluster = string.(day2.spmetaData.cell.cluster)
+day2.polygonData = polygons
+day2 = cs.polygons_cell_mapping(day2)
+day2 = cs.generate_polygon_counts(day2)
+cs.save(day2; filename="day2.jld2")
+
+### 5. IRI6w ###
+count_molecules =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Week6_Jia/segmentation.csv"))
+count_df =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Week6_Jia/segmentation_counts.tsv"))
+count_cells =  DataFrame(CSV.File("/mnt/sdc/new_analysis_cellscopes/seg_files/Week6_Jia/segmentation_cell_stats.csv"))
+import Baysor as B
+scale=30
+min_pixels_per_cell = 15
+grid_step = scale / min_pixels_per_cell
+bandwidth= scale / 10
+polygons = B.boundary_polygons(count_molecules, count_molecules.cell, grid_step=grid_step, bandwidth=bandwidth)
+genes = count_df.gene
+cells = string.(names(count_df)[2:end])
+count_df = count_df[!, 2:end]
+count_df = convert(SparseMatrixCSC{Int64, Int64},Matrix(count_df))
+raw_count = cs.RawCountObject(count_df, cells, genes)
+count_molecules.cell = string.(count_molecules.cell)
+count_cells.cell = string.(count_cells.cell)
+week6 = cs.CartanaObject(count_molecules, count_cells, raw_count;
+    prefix = "week6", min_gene = 0, min_cell = 3)
+week6 = cs.normalize_object(week6)
+week6 = cs.scale_object(week6)
+week6 = cs.find_variable_genes(week6; nFeatures=100)
+week6 = cs.run_pca(week6;  method=:svd, pratio = 1, maxoutdim = 20)
+week6.metaData.cluster = string.(week6.spmetaData.cell.cluster)
+week6.polygonData = polygons
+week6 = cs.polygons_cell_mapping(week6)
+week6 = cs.generate_polygon_counts(week6)
+cs.save(week6; filename="week6.jld2")
+
+
+
